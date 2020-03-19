7da4d08f418f4109dc45ecc31f66129a48fc09fe
Hassan Algadah <45927520+HassanAlgadah@users.noreply.github.com>
2020-03-19 20:06:58 +0300

    added view engine

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const socket = require('socket.io');
+const hbs = require('hbs');
 const app = express();
 const server = app.listen(400,()=> console.log("listening 400"));
 
-app.use(express.static('public'));
+app.set('view engine','hbs')
+// app.use(express.static('public'));
 
 let io = socket(server);
+
+
+//test the database here !!!
+app.get('/login',(req,res)=>{
+    res.render("login")
+});
+
+
 
 io.on('connection',(socket)=> {
     socket.broadcast.emit('opid',{
